5eda4e13731e47828f152c03887eeb80374327e7
ihatomorio <ihatomorio@hotmail.co.jp>
2021-07-19 15:00:11 +0900

    Remove unused

--- src/dummy-nic.c
+++ src/dummy-nic.c
@@ -22,13 +22,6 @@
                        }
 
 
-uint16_t checksum(uint16_t *buf, size_t buflen);
-int make_udp_packet(int16_t src_port, int16_t dst_port, void **udp_packet, void *udp_payload, size_t udp_payload_len);
-int make_ip_packet(uint16_t id, uint8_t protocol, const char *src_ip, const char *dst_ip, void **ip_packet, void *data_buf, size_t data_buf_len);
-int make_ether_frame(void **ether_frame, size_t *ether_frame_len, void *data, size_t data_len);
-void print_hex(void *buf, size_t buflen);
-
-
 int main(int argc, char *argv[])
 {
     int exit_status = EXIT_SUCCESS;
@@ -66,58 +59,4 @@ int main(int argc, char *argv[])
     close(socket_descriptor);
 final:
     return exit_status;
-}
-
-
-uint16_t checksum(uint16_t *buf, size_t buflen)
-{
-    unsigned long sum = 0;
-    uint16_t checksum;
-
-    while (buflen > 1)
-    {
-        sum += *buf;
-        buf++;
-        buflen -= 2;
-    }
-
-    if (buflen == 1)
-    {
-        sum += *(unsigned char *)buf;
-    }
-
-    sum = (sum & 0xffff) + (sum >> 16);
-    sum = (sum & 0xffff) + (sum >> 16);
-
-    checksum = (uint16_t)~sum;
-
-    return checksum;
-}
-
-
-
-
-void print_hex(void *buf, size_t buflen)
-{
-    uint8_t *poiner = (uint8_t *)buf;
-    size_t offset = 0;
-
-    for (offset = 0; offset < buflen; offset++)
-    {
-        printf("%02x", (uint8_t)*poiner);
-        poiner++;
-
-        if (offset % 16 == 15)
-        {
-            printf("\n");
-            continue;
-        }
-
-        if (offset % 2 == 1)
-        {
-            printf(" ");
-        }
-    }
-
-    printf("\n");
 }
